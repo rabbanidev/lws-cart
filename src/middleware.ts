2ab258f165dc2e23cf57307e468c134b97e9297b
rabbanidev <golamrabbani227417@gmail.com>
2024-05-23 22:37:34 +0600

    cookies debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -72,7 +72,9 @@ import { getLocale } from '../utils/getLocale';
 import { getLanguageFromURL, removeLanguagePrefix } from '../utils/url';
 import { loggedInRoutes, loggedOutRoutes } from '../constants';
 import { getToken } from 'next-auth/jwt';
+import { parse } from 'cookie';
 import envConfig from '../config/envConfig';
+import { cookies } from 'next/headers';
 
 export const middleware = async (request: NextRequest) => {
   const { pathname } = request.nextUrl;
@@ -86,10 +88,23 @@ export const middleware = async (request: NextRequest) => {
     secret: process.env.AUTH_SECRET,
   });
 
+  const cookie = parse(request.headers.get('cookie') || '');
+  const token2 =
+    cookie['authjs.session-token'] || cookie['__Secure-authjs.session-token'];
+
+  const cookieStore = cookies();
+  const token3 =
+    cookieStore.get('__Secure-authjs.session-token') ||
+    cookieStore.get('authjs.session-token');
+
   // Enhanced logging for debugging
-  console.log('Environment Variables:', process.env);
+  // console.log('Environment Variables:', process.env);
   console.log('Token:', token);
   console.log('envConfig.auth.secret:', envConfig.auth.secret);
+  console.log('token2:', token2);
+  console.log('token3:', token3);
+  console.log('custom-cookie:', cookieStore.get('custom-cookie'));
+  console.log('all-cookie:', cookieStore.getAll());
 
   const pathNameIsMissingLocale = i18n.locales.every(
     (locale) =>
