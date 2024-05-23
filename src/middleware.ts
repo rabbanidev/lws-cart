4b814ef719da4011f46e90f0b4da7ae2f7e73fe6
rabbanidev <golamrabbani227417@gmail.com>
2024-05-23 22:46:29 +0600

    token debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -71,7 +71,6 @@ import type { NextRequest } from 'next/server';
 import { getLocale } from '../utils/getLocale';
 import { getLanguageFromURL, removeLanguagePrefix } from '../utils/url';
 import { loggedInRoutes, loggedOutRoutes } from '../constants';
-import { getToken } from 'next-auth/jwt';
 import { parse } from 'cookie';
 import envConfig from '../config/envConfig';
 import { cookies } from 'next/headers';
@@ -81,28 +80,20 @@ export const middleware = async (request: NextRequest) => {
   const lang = getLanguageFromURL(pathname);
   const actualPathname = removeLanguagePrefix(pathname);
 
-  const token = await getToken({
-    req: request,
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    secret: process.env.AUTH_SECRET,
-  });
-
   const cookie = parse(request.headers.get('cookie') || '');
   const token2 =
     cookie['authjs.session-token'] || cookie['__Secure-authjs.session-token'];
 
   const cookieStore = cookies();
-  const token3 =
+  const token =
     cookieStore.get('__Secure-authjs.session-token') ||
     cookieStore.get('authjs.session-token');
 
   // Enhanced logging for debugging
   // console.log('Environment Variables:', process.env);
-  console.log('Token:', token);
   console.log('envConfig.auth.secret:', envConfig.auth.secret);
   console.log('token2:', token2);
-  console.log('token3:', token3);
+  console.log('token3:', token);
   console.log('custom-cookie:', cookieStore.get('custom-cookie'));
   console.log('all-cookie:', cookieStore.getAll());
 
@@ -119,14 +110,14 @@ export const middleware = async (request: NextRequest) => {
   }
 
   if (
-    !token &&
+    !token?.value &&
     loggedInRoutes.some((path) => actualPathname.startsWith(path))
   ) {
     return NextResponse.redirect(
       new URL(`/${lang}/login?next=${pathname}`, request.nextUrl),
     );
   } else if (
-    token &&
+    token?.value &&
     loggedOutRoutes.some((path) => actualPathname.startsWith(path))
   ) {
     return NextResponse.redirect(new URL(`/${lang}/account`, request.nextUrl));
