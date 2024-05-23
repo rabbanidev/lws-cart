f3af1ac467ab80a5cf931d24e5f02678e34b1851
rabbanidev <golamrabbani227417@gmail.com>
2024-05-23 22:17:32 +0600

    env log

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,101 @@
+// import { NextResponse } from 'next/server';
+// import { i18n } from '@/i18n.config';
+// import type { NextRequest } from 'next/server';
+// import { getLocale } from '../utils/getLocale';
+// // import { cookies } from 'next/headers';
+// import { getLanguageFromURL, removeLanguagePrefix } from '../utils/url';
+// import { loggedInRoutes, loggedOutRoutes } from '../constants';
+// import { getToken } from 'next-auth/jwt';
+// import envConfig from '../config/envConfig';
+
+// export const middleware = async (request: NextRequest) => {
+//   // const cookieStore = cookies();
+//   const { pathname } = request.nextUrl;
+//   const lang = getLanguageFromURL(pathname);
+//   const actualPathname = removeLanguagePrefix(pathname);
+//   // const token = cookieStore.get(
+//   //   'authjs.session-token' || '__Secure-authjs.session-token',
+//   // )?.value;
+
+//   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//   // @ts-ignore
+//   const token = await getToken({ req: request, secret: envConfig.auth.secret });
+
+//   console.log('token', token);
+//   console.log('envConfig.auth.secret', envConfig.auth.secret);
+
+//   const pathNameIsMissingLocale = i18n.locales.every(
+//     (locale) =>
+//       !pathname.startsWith(`/${locale}`) && !pathname.startsWith(`/${locale}/`),
+//   );
+
+//   //TODO: Redirect if there is no locale
+//   if (pathNameIsMissingLocale) {
+//     const locale = getLocale(request);
+//     return NextResponse.redirect(
+//       new URL(`/${locale}/${pathname}`, request.nextUrl),
+//     );
+//   }
+
+//   // TODO: private and public route handle
+//   if (
+//     !token &&
+//     loggedInRoutes.some((path) => actualPathname.startsWith(path))
+//   ) {
+//     return NextResponse.redirect(
+//       new URL(`/${lang}/login?next=${pathname}`, request.nextUrl),
+//       // new URL(`/${lang}/login`, request.nextUrl),
+//     );
+//   } else if (
+//     token &&
+//     loggedOutRoutes.some((path) => actualPathname.startsWith(path))
+//   ) {
+//     return NextResponse.redirect(new URL(`/${lang}/account`, request.nextUrl));
+//   }
+
+//   return NextResponse.next();
+// };
+
+// export const config = {
+//   matcher: [
+//     // Skip all internal paths (_next, assets, api)
+//     '/((?!api|assets|.*\\..*|_next).*)',
+//     // Optional: only run on root (/) URL
+//     // '/'
+//   ],
+// };
+
 import { NextResponse } from 'next/server';
 import { i18n } from '@/i18n.config';
 import type { NextRequest } from 'next/server';
 import { getLocale } from '../utils/getLocale';
-// import { cookies } from 'next/headers';
 import { getLanguageFromURL, removeLanguagePrefix } from '../utils/url';
 import { loggedInRoutes, loggedOutRoutes } from '../constants';
 import { getToken } from 'next-auth/jwt';
 import envConfig from '../config/envConfig';
 
 export const middleware = async (request: NextRequest) => {
-  // const cookieStore = cookies();
   const { pathname } = request.nextUrl;
   const lang = getLanguageFromURL(pathname);
   const actualPathname = removeLanguagePrefix(pathname);
-  // const token = cookieStore.get(
-  //   'authjs.session-token' || '__Secure-authjs.session-token',
-  // )?.value;
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const token = await getToken({ req: request, secret: envConfig.auth.secret });
+  const token = await getToken({
+    req: request,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    secret: process.env.AUTH_SECRET,
+  });
 
-  console.log('token', token);
-  console.log('envConfig.auth.secret', envConfig.auth.secret);
+  // Enhanced logging for debugging
+  console.log('Environment Variables:', process.env);
+  console.log('Token:', token);
+  console.log('envConfig.auth.secret:', envConfig.auth.secret);
 
   const pathNameIsMissingLocale = i18n.locales.every(
     (locale) =>
       !pathname.startsWith(`/${locale}`) && !pathname.startsWith(`/${locale}/`),
   );
 
-  //TODO: Redirect if there is no locale
   if (pathNameIsMissingLocale) {
     const locale = getLocale(request);
     return NextResponse.redirect(
@@ -37,14 +103,12 @@ export const middleware = async (request: NextRequest) => {
     );
   }
 
-  // TODO: private and public route handle
   if (
     !token &&
     loggedInRoutes.some((path) => actualPathname.startsWith(path))
   ) {
     return NextResponse.redirect(
       new URL(`/${lang}/login?next=${pathname}`, request.nextUrl),
-      // new URL(`/${lang}/login`, request.nextUrl),
     );
   } else if (
     token &&
@@ -57,10 +121,5 @@ export const middleware = async (request: NextRequest) => {
 };
 
 export const config = {
-  matcher: [
-    // Skip all internal paths (_next, assets, api)
-    '/((?!api|assets|.*\\..*|_next).*)',
-    // Optional: only run on root (/) URL
-    // '/'
-  ],
+  matcher: ['/((?!api|assets|.*\\..*|_next).*)'],
 };
